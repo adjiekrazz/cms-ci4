00b9e3ddcc9cf01919e20381cc1d3da6f94b572e
Arif Purnomo Aji <arif.purnomo@lidahbuayagroup.com>
2022-01-23 09:05:10 +0700

    refactor category controller

--- app/Controllers/Category.php
+++ app/Controllers/Category.php
@@ -27,90 +27,83 @@ class Category extends BaseController
 	{
 		if (! has_permission('read')){
             return $this->failForbidden("You don't have permissions to view resources.");
-        } else {
-            $search = $this->request->getVar('search')['value'];
-            $limit = $this->request->getVar('length');
-            $start = $this->request->getVar('start');
-            $order_index = $this->request->getVar('order')[0]['column'];
-            $order_field = $this->request->getVar('columns')[$order_index]['data'];
-            $order_ascdesc = $this->request->getVar('order')[0]['dir'];
-            $categories_query = $this->model->orderBy($order_field, $order_ascdesc);
-            if ($search) {
-                $categories_query = $categories_query->like('name', $search);
-                $categories_query = $categories_query->orLike('slug', $search);
-            }
-            $categories_data = $categories_query->findAll($limit, $start);
-            $categories_filter_total = $categories_query->countAll();
-            $categories_total = $this->model->countAll();
-
-            $callback = array(
-                'draw' => $this->request->getVar('draw'),
-                'recordsTotal' => $categories_total,
-                'recordsFiltered' => $categories_filter_total,
-                'data' => $categories_data,
-            );
-
-            header('Content-Type: application/json');
-            echo json_encode($callback);
         }
+
+        $search = $this->request->getVar('search')['value'];
+        $limit = $this->request->getVar('length');
+        $start = $this->request->getVar('start');
+        $order_index = $this->request->getVar('order')[0]['column'];
+        $order_field = $this->request->getVar('columns')[$order_index]['data'];
+        $order_ascdesc = $this->request->getVar('order')[0]['dir'];
+        $categories_query = $this->model->orderBy($order_field, $order_ascdesc);
+        if ($search) {
+            $categories_query = $categories_query->like('name', $search);
+            $categories_query = $categories_query->orLike('slug', $search);
+        }
+        $categories_data = $categories_query->findAll($limit, $start);
+        $categories_filter_total = $categories_query->countAll();
+        $categories_total = $this->model->countAll();
+
+        $callback = array(
+            'draw' => $this->request->getVar('draw'),
+            'recordsTotal' => $categories_total,
+            'recordsFiltered' => $categories_filter_total,
+            'data' => $categories_data,
+        );
+
+        header('Content-Type: application/json');
+        echo json_encode($callback);
 	}
 
 	public function addCategory()
     {
-        if (! has_permission('create')){
+        if (! has_permission('create'))
             return $this->failForbidden("You don't have permissions to create new resources.");
-        } else {
-            $categories_data = $this->request->getRawInput();
-            
-            $this->validation->setRules($this->model->validationRules);
-            if ($this->validation->run($categories_data))
-            {
-                $this->model->save($categories_data);
-                return $this->respondCreated($categories_data);
-            } else {
-                return $this->failValidationErrors($this->validation->getErrors());
-            }
-        }
+
+		$categories_data = $this->request->getRawInput();
+		$this->validation->setRules($this->model->validationRules);
+
+		if (! $this->validation->run($categories_data))
+			return $this->failValidationErrors($this->validation->getErrors());
+
+		if ($this->model->save($categories_data))
+		    return $this->respondCreated($categories_data);
+        
+        return $this->fail(new \CodeIgniter\Database\Exceptions\DatabaseException());
     }
 
     public function editCategory()
     {
-        if (! has_permission('update')){
+        if (! has_permission('update'))
             return $this->failForbidden("You don't have permissions to edit resources.");
-        } else {
-            $categories_data = [
-                'name' => $this->request->getVar('name'),
-                'slug' => $this->request->getVar('slug'),
-            ];
+        
+        $categories_data = [
+            'name' => $this->request->getVar('name'),
+            'slug' => $this->request->getVar('slug'),
+        ];
 
-            $this->validation->setRules($this->model->validationRules);
-            if ($this->validation->run($categories_data))
-            {
-                if ($this->model->update($this->request->getVar('id'), $categories_data)) {
-                    return $this->respondCreated($categories_data);
-                } else {
-                    return $this->fail(new \CodeIgniter\Database\Exceptions\DatabaseException()); 
-                }
-            } else {
-                return $this->failValidationErrors($this->validation->getErrors());
-            }
-        }
+        $this->validation->setRules($this->model->validationRules);
+        if (!$this->validation->run($categories_data))
+            return $this->failValidationErrors($this->validation->getErrors());
+    
+        if ($this->model->update($this->request->getVar('id'), $categories_data))
+            return $this->respondCreated($categories_data);
+        
+        return $this->fail(new \CodeIgniter\Database\Exceptions\DatabaseException()); 
+            
     }
 
     public function deleteCategory($id = null)
     {
         if ($id === null)
-        {
             return $this->failNotFound('Category ID cannot be null');
-        }
 
-        if (! has_permission('delete')){
+        if (! has_permission('delete'))
             return $this->failForbidden("You don't have permissions to delete resources.");
-        } else {
-            if ($this->model->delete($id))
-            {
-                $this->respondDeleted($id);
-            }
-        }
+        
+        if ($this->model->delete($id))
+            return $this->respondDeleted($id);
+        
+        return $this->fail(new \CodeIgniter\Database\Exceptions\DatabaseException()); 
     }
 }
