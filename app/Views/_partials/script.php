01c6edccf7d07c1256ba2f12cfed87a8718bf75b
Arif Purnomo Aji <arif.purnomo@lidahbuayagroup.com>
2022-01-25 15:17:56 +0700

    add escapeHtml function

--- app/Views/_partials/script.php
+++ app/Views/_partials/script.php
@@ -6,3 +6,13 @@
 <script src="../../plugins/toastr/toastr.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?= base_url('js/adminlte.min.js') ?>"></script>
+<script type="text/javascript">
+    function escapeHtml(unsafe){
+        return unsafe
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
+</script>
