e9743e81639d52d963a074d171bbd59c94181ddb
Arif Purnomo Aji <tibiame.cheatplatinum@gmail.com>
2022-01-31 23:35:18 +0700

    complete dashboard module

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2,10 +2,34 @@
 
 namespace App\Controllers;
 
+use App\Models\ArticleModel;
+use App\Models\CategoryModel;
+use App\Models\PageModel;
+use App\Models\UserModel;
+
 class Dashboard extends BaseController
 {
+	protected $articleModel;
+	protected $categoryModel;
+	protected $userModel;
+	protected $pageModel;
+
+	public function __construct()
+	{
+		$this->articleModel = new ArticleModel();
+		$this->categoryModel = new CategoryModel();
+		$this->userModel = new UserModel();
+		$this->pageModel = new PageModel();
+	}
+
 	public function index()
 	{
-		return view('dashboard');
+		$data = [
+			'articles' => $this->articleModel->countAllResults(),
+			'categories' => $this->categoryModel->countAllResults(),
+			'users' => $this->userModel->countAllResults(),
+			'pages' => $this->pageModel->countAllResults(),
+		];
+		return view('dashboard', $data);
 	}
 }
--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -21,9 +21,68 @@
 
         <section class="content">
             <div class="container-fluid">
-                <div class="card card-solid">
-                    <div class="card-body">
-                          <p>Welcome to Content Management System made with Codeigniter 4!</p>
+                <div class="row">
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3><?= $articles ?></h3>
+
+                            <p>Total Articles</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="<?= base_url('article') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3><?= $categories ?></h3>
+
+                            <p>Total Category</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-stats-bars"></i>
+                        </div>
+                        <a href="<?= base_url('category') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3><?= $users ?></h3>
+
+                            <p>Total Users</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person-add"></i>
+                        </div>
+                        <a href="<?= base_url('user') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3><?= $pages ?></h3>
+
+                            <p>Total Pages</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-pie-graph"></i>
+                        </div>
+                        <a href="<?= base_url('page') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5>Welcome, <?= user()->username . ' [' . user()->email . ']' ?></h5>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
