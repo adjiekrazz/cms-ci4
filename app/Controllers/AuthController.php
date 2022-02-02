f10be7c16e1569b7bd88e0fa50395a6ea94c5c0f
Arif Purnomo Aji <tibiame.cheatplatinum@gmail.com>
2022-02-02 14:55:21 +0700

    change redirect url to backend after login and logout

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -96,7 +96,7 @@ class AuthController extends Controller
 			return redirect()->to(route_to('reset-password') .'?token='. $this->auth->user()->reset_hash)->withCookies();
 		}
 
-		$redirectURL = session('redirect_url') ?? site_url('/');
+		$redirectURL = session('redirect_url') ?? site_url('backend');
 		unset($_SESSION['redirect_url']);
 
 		return redirect()->to($redirectURL)->withCookies()->with('message', lang('Auth.loginSuccess'));
@@ -112,7 +112,7 @@ class AuthController extends Controller
 			$this->auth->logout();
 		}
 
-		return redirect()->to(site_url('/'));
+		return redirect()->to(site_url('backend'));
 	}
 
 	//--------------------------------------------------------------------
