b112ff187fcbfc26449ce209add7c6b6ed0a1ff3
Arif Purnomo Aji <tibiame.cheatplatinum@gmail.com>
2022-02-06 11:33:50 +0700

    add content to PagesSeeder

--- app/Database/Seeds/PagesSeeder.php
+++ app/Database/Seeds/PagesSeeder.php
@@ -13,17 +13,20 @@ class PagesSeeder extends Seeder
 			[
 				'title' => 'Services',
 				'slug' => 'page/services',
-				'content' => '',
+				'content' => '<ol><li>Web Design<br><p>We provide web design services using figma or other prototyping applications.</p></li>'
+					. '<li>Automation<br><p>Automate your server health recovery and extract error logs for further analysis.</p></li></ol>',
 			],
 			[
 				'title' => 'About',
 				'slug' => 'page/about',
-				'content' => '',
+				'content' => 'APA Daily is a place for me to share whatever in my mind about technology.',
 			],
 			[
 				'title' => 'Contact',
 				'slug' => 'page/contact',
-				'content' => '',
+				'content' => '<blockquote class="blockquote"><p><b>WhatsApp </b>: +62 882 2184 xxxx&nbsp;<br>'
+					. '<b style="font-size: 1.25rem;">Email&nbsp;</b><span style="font-size: 1.25rem;">: admin@apadaily.com&nbsp; </span><br>'
+					. '<b style="font-size: 1.25rem;">Alamat </b><span style="font-size: 1.25rem;">: Jl Secang Temanggung&nbsp;</span></p></blockquote>',
 			],
 		];
 
